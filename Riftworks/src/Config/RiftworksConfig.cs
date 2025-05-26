b2b0f97c841b2a7e4519a9ae973f9bab79f96585
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2025-05-26 15:13:54 +0200

    Added config to disable recipes. All are on by default.

---
+++ Riftworks/src/Config/RiftworksConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riftworks.src.Config
+{
+    public class RiftworksConfig
+    {
+        public static RiftworksConfig Loaded { get; set; } = new RiftworksConfig();
+        
+        public bool DisableTemporalDisassembler { get; set; } = false;
+        public bool DisableStormCaster { get; set; } = false;
+
+        public bool DisableVectorStasisUnit { get; set; } = false;
+
+        public bool DisableDivingHelmet { get; set; } = false;
+
+        //public bool DisableRiftBlade { get; set; } = false;
+        //public bool DisableAdaptiveReconstitutionGear { get; set; } = false;
+        //public bool DisableOreScanner { get; set; } = false;
+        //public bool DisableGravityBoots { get; set; } = false;
+        //public bool DisableAdaptiveReconstitutionGearEntity { get; set; } = false;
+
+    }
+}
--- Riftworks/src/RiftworksModSystem.cs
+++ Riftworks/src/RiftworksModSystem.cs
@@ -1,17 +1,23 @@
 ﻿using Riftworks.src.BE;
 using Riftworks.src.Blocks;
+using Riftworks.src.Config;
 using Riftworks.src.Entities;
 using Riftworks.src.Items;
 using Riftworks.src.Items.Wearable;
+using System;
+using System.Collections.Generic;
 using Vintagestory.API.Common;
 
 namespace Riftworks.src;
 
 public class RiftworksModSystem : ModSystem
 {
+    const string CONFIG_FILE = "riftworks.json";
 
     public override void Start(ICoreAPI api)
     {
+        TryLoadConfig(api);
+
         api.RegisterBlockClass($"{Mod.Info.ModID}:BlockTemporalDisassembler", typeof(BlockTemporalDisassembler));
         api.RegisterBlockEntityClass($"{Mod.Info.ModID}:BETemporalDisassembler", typeof(BlockEntityTemporalDisassembler));
 
@@ -27,4 +33,43 @@ public class RiftworksModSystem : ModSystem
 
         api.RegisterEntity("EntityAdaptiveReconstitutionGear", typeof(EntityAdaptiveReconstitutionGear));
     }
+
+    void TryLoadConfig(ICoreAPI api)
+    {
+        try
+        {
+            RiftworksConfig.Loaded = api.LoadModConfig<RiftworksConfig>(CONFIG_FILE) ?? new RiftworksConfig();
+            api.StoreModConfig<RiftworksConfig>(RiftworksConfig.Loaded, CONFIG_FILE);
+        }
+        catch (Exception e)
+        {
+            api.Logger.Error("Could not load Riftworks config, using defaults.", e);
+            RiftworksConfig.Loaded = new RiftworksConfig();
+        }
+    }
+
+    // After all JSON assets have been loaded, remove any configurated recipes
+    public override void AssetsFinalize(ICoreAPI api)
+    {
+        if (api.Side != EnumAppSide.Server) return;
+
+        List<GridRecipe> recipes = api.World.GridRecipes;
+
+        if (RiftworksConfig.Loaded.DisableDivingHelmet)
+        {
+            recipes.RemoveAll(r => r.Output.Code.PathStartsWith("divinghelmet"));
+        }
+        if (RiftworksConfig.Loaded.DisableVectorStasisUnit)
+        {
+            recipes.RemoveAll(r => r.Output.Code.PathStartsWith("vectorstasisunit"));
+        }
+        if (RiftworksConfig.Loaded.DisableStormCaster) { 
+            recipes.RemoveAll(r => r.Output.Code.PathStartsWith("stormcaster"));
+        }
+        if (RiftworksConfig.Loaded.DisableTemporalDisassembler)
+        {
+            recipes.RemoveAll(r => r.Output.Code.PathStartsWith("temporaldisassembler"));
+        }
+    }
+
 }
