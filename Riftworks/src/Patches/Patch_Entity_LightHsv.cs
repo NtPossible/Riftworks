de5a3819ccb159d84dc43303939f090dbc0e1426
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2025-05-28 22:48:47 +0200

    readability

--- Riftworks/src/Patches/Patch_Entity_LightHsv.cs
+++ Riftworks/src/Patches/Patch_Entity_LightHsv.cs
@@ -9,12 +9,18 @@ namespace Riftworks.src.Patches
     {
         static bool Prefix(Entity __instance, ref byte[] __result)
         {
-            if (__instance is not EntityPlayer entityPlayer) return true;
-
+            if (__instance is not EntityPlayer entityPlayer)
+            {
+                return true;
+            }
+            
             // Only override if helmet light flag is enabled
             bool helmetLightEnabled = entityPlayer.WatchedAttributes.GetBool("riftworksHelmetLight");
-            if (!helmetLightEnabled) return true;
-
+            if (!helmetLightEnabled)
+            {
+                return true;
+            }
+            
             // Emit light
             __result = new byte[] { 25, 5, 5 };
             return false; // Skip original getter
