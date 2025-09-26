0a71019553c77dbe44827c687e175dde2d87216f
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2025-09-26 22:37:10 +0200

    added item descriptions

--- Riftworks/src/Items/Wearable/ItemDivingHelmet.cs
+++ Riftworks/src/Items/Wearable/ItemDivingHelmet.cs
@@ -7,10 +7,6 @@ namespace Riftworks.src.Items.Wearable
 {
     public class ItemDivingHelmet : ItemWearable
     {
-        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
-        {
-            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
-            dsc.AppendLine(Lang.Get("Activates light and increases breath time to 5 minutes when underwater."));
-        }
+
     }
 }
--- Riftworks/src/Items/Wearable/ItemOreScanner.cs
+++ Riftworks/src/Items/Wearable/ItemOreScanner.cs
@@ -12,7 +12,6 @@ namespace Riftworks.src.Items.Wearable
         public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool debug)
         {
             base.GetHeldItemInfo(inSlot, dsc, world, debug);
-            dsc.AppendLine(Lang.Get("Scans for ores within 10 blocks"));
         }
     }
 }
